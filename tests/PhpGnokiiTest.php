bff907ef09f9603b1150a3e30af39a34cf74e879
root <root@localhost.localdomain>
2017-09-02 22:08:27 +0800

    initial commit

---
+++ tests/PhpGnokiiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+
+use \AM\SMS\PhpGnokii;
+use \PHPUnit\Framework\TestCase;
+
+class PhpGnokiiTest extends TestCase
+    {
+
+	/**
+	 * Should allow to send sms
+	 *
+	 * @return void
+	 */
+
+	public function testShouldAllowToSendSms()
+	    {
+	    } //end testShouldAllowToSendSms()
+
+
+    } //end class
+
+?>
