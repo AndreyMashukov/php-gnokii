b65b1551f3ab73c80d5228598e506bebc4518a2a
root <root@localhost.localdomain>
2017-09-02 23:41:44 +0800

    Added tests and sender (PhpGnokii::class)

---
+++ src/PhpGnokii.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace AM\SMS;
+
+use \Exception;
+
+class PhpGnokii
+    {
+
+	/**
+	 * Sms center number
+	 *
+	 * @var string
+	 */
+	private $_smsc;
+
+	/**
+	 * Country code
+	 *
+	 * @var string
+	 */
+	private $_code;
+
+	/**
+	 * Prepare php gnokii to work
+	 *
+	 * @param string $smsc        Sms center number
+	 * @param string $countrycode Country phone code
+	 *
+	 * @return void
+	 *
+	 * @throws Exception Invalid SMSC number
+	 *
+	 * @exceptioncode EXCEPTION_INVALID_SMSC_NUMBER
+	 */
+
+	public function __construct(string $smsc = "", $countrycode = "+7")
+	    {
+		$this->_code = $countrycode;
+
+		if ($smsc === "" && defined("SMSC_NUMBER") === true)
+		    {
+			$smsc = SMSC_NUMBER;
+		    } //end if
+
+		if ($this->_validatePhoneNumber($smsc) === true)
+		    {
+			$this->_smsc = $smsc;
+		    }
+		else
+		    {
+			throw new Exception("Invalid SMSC number", EXCEPTION_INVALID_SMSC_NUMBER);
+		    } //end if
+
+	    } //end __construct()
+
+
+	/**
+	 * Validate phone number
+	 *
+	 * @param string $phonenumber Mobile phone number
+	 *
+	 * @return bool Validate result
+	 */
+
+	private function _validatePhoneNumber(string $phonenumber):bool
+	    {
+		return ((preg_match("/^" . str_replace("+", "\+", $this->_code) . "[0-9]{10}$/ui", $phonenumber) > 0) ? true : false);
+	    } //end _validatePhoneNumber()
+
+
+	/**
+	 * Send message
+	 *
+	 * @param string $phone   Phone number to send
+	 * @param string $message Message to send
+	 *
+	 * @return bool Send result
+	 */
+
+	public function send(string $phone, string $message):bool
+	    {
+		if ($this->_validatePhoneNumber($phone) === true)
+		    {
+			exec("echo '" . $message . "' | gnokii --sendsms '" . $phone . "' --smsc '" . $this->_smsc . "'", $output, $result);
+			if ($result === 0)
+			    {
+				return true;
+			    }
+			else
+			    {
+				return false;
+			    } //end if
+
+		    }
+		else
+		    {
+			return false;
+		    } //end if
+
+	    } //end send()
+
+    } //end class
+
+
+?>
--- tests/PhpGnokiiTest.php
+++ tests/PhpGnokiiTest.php
@@ -4,9 +4,27 @@ namespace Tests;
 
 use \AM\SMS\PhpGnokii;
 use \PHPUnit\Framework\TestCase;
+use \Exception;
 
 class PhpGnokiiTest extends TestCase
     {
+
+	/**
+	 * Should not allow create sender without smsc number
+	 *
+	 * @return void
+	 *
+	 * @exceptioncode EXCEPTION_INVALID_SMSC_NUMBER
+	 */
+
+	public function testShouldNotAllowCreateSenderWithoutSmscNumber()
+	    {
+		define("EXCEPTION_INVALID_SMSC_NUMBER", 1);
+		$this->expectException(Exception::class);
+		$this->expectExceptionCode(EXCEPTION_INVALID_SMSC_NUMBER);
+		$sender = new PhpGnokii();
+	    } //end testShouldNotAllowCreateSenderWithoutSmscNumber
+
 
 	/**
 	 * Should allow to send sms
@@ -16,6 +34,10 @@ class PhpGnokiiTest extends TestCase
 
 	public function testShouldAllowToSendSms()
 	    {
+		define("SMSC_NUMBER", "+79043490000");
+
+		$sender = new PhpGnokii();
+		$this->assertTrue($sender->send("+79526191914", "test"));
 	    } //end testShouldAllowToSendSms()
 
 
